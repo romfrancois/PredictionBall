61e2bf9cf5aae6522f079e8cf464492fb76eabef
Romain Francois <romain.francois@gmail.com>
2020-06-17 09:42:33 +0100

    Adding asset & all the logic to randomly display a different quote.

--- PredictionBall/ViewController.swift
+++ PredictionBall/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let answers = [
+        "As I see it, yes.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don’t count on it.",
+        "It is certain.",
+        "It is decidedly so.",
+        "Most likely.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",
+        "Outlook good.",
+        "Reply hazy, try again.",
+        "Signs point to yes.",
+        "Very doubtful.",
+        "Without a doubt.",
+        "Yes.",
+        "Yes – definitely.",
+        "You may rely on it."
+    ]
+    
+    @IBOutlet weak var eightBallAnswer: UILabel!    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func answerMyQuestionPressed(_ sender: UIButton) {
+        eightBallAnswer.text = answers.randomElement()
+    }
+    
 }
 
